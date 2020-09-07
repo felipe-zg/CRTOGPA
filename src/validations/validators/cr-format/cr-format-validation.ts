a3481431b06d0adab9c173998872343fab7ecbdd
felipe-zg <felipe_zeba@outlook.com>
2020-09-07 09:56:39 -0300

    feat: ensure CRFormatValidation returns InvalidCRFormatError if CR is invalid

---
+++ src/validations/validators/cr-format/cr-format-validation.ts
@@ -0,0 +1,9 @@
+import { FieldValidation } from '@/validations/protocols'
+import { InvalidCRFormatError } from '@/domain/errors'
+
+export class CRFormatValidation implements FieldValidation {
+  constructor(readonly field: string) {}
+  validate(cr: number): Error {
+    return cr <= 0 || cr > 10 ? new InvalidCRFormatError() : null
+  }
+}
