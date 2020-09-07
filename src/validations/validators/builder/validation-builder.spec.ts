7b9e82a10e521c9b6bbc11cfbec7267ceff3ad57
felipe-zg <felipe_zeba@outlook.com>
2020-09-07 12:41:59 -0300

    test: ensure ValidationBuilder return CRFormatValidation

--- src/validations/validators/builder/validation-builder.spec.ts
+++ src/validations/validators/builder/validation-builder.spec.ts
@@ -1,11 +1,17 @@
 import faker from 'faker'
 import { ValidationBuilder as sut } from '@/validations/validators/builder/validation-builder'
-import { RequiredFieldValidation } from '../required-field/required-field-validation'
+import { RequiredFieldValidation, CRFormatValidation } from '@/validations/validators'
 
 describe('ValidationBuilder', () => {
   it('should return  RequiredFieldValidation', () => {
     const field = faker.database.column()
     const validators = sut.field(field).required().build()
     expect(validators).toEqual([new RequiredFieldValidation(field)])
+  })
+
+  it('should return CRFormatValidation', () => {
+    const field = faker.database.column()
+    const validators = sut.field(field).cr().build()
+    expect(validators).toEqual([new CRFormatValidation(field)])
   })
 })
