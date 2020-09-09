f1c19f88d147268cc5ea9e93c440f445ade4714b
felipe-zg <felipe_zeba@outlook.com>
2020-09-09 13:02:50 -0300

    feat: added context to the form

--- src/presentation/pages/home/home.tsx
+++ src/presentation/pages/home/home.tsx
@@ -1,27 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Context from '@/presentation/context/form/form-context'
 import * as S from './styles.scss'
 
 import { Input } from '@/presentation/components'
 
 const home: React.FC = () => {
+  const [state, setState] = useState({
+    cr: '',
+    crError: '',
+    errorMessage: ''
+  })
+
   return (
     <div className={S.home}>
       <header className={S.header}>
         <h1>LOGO</h1>
       </header>
-      <main className={S.main}>
-        <h2>Seu CR</h2>
+      <Context.Provider value={{ state, setState }}>
+        <form className={S.form}>
+          <h2>Seu CR</h2>
 
-        <Input type="text"/>
+          <Input type="text" name="cr"/>
 
-        <button type="button" className={S.submit}>Converter</button>
+          <button type="button" className={S.submit}>Converter</button>
 
-        <div className={S.convertionResult}>
-          <p>3.7</p>
-        </div>
+          <div className={S.convertionResult}>
+            <p>3.7</p>
+          </div>
 
-        <h3>Parabéns ! Seu GPA é ótimo</h3>
-      </main>
+          <h3>Parabéns ! Seu GPA é ótimo</h3>
+        </form>
+      </Context.Provider>
 
       <footer className="footer">
         <p>created by Felipe zeba</p>
