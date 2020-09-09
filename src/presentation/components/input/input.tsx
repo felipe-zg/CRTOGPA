70dc79c6ce081211ad85434d0ba77533838369a3
felipe-zg <felipe_zeba@outlook.com>
2020-09-09 13:03:12 -0300

    feat: added context to the input component

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import Context from '@/presentation/context/form/form-context'
 
 import * as S from './styles.scss'
 
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 const input: React.FC<Props> = (props: Props) => {
+  const { state, setState } = useContext(Context)
+  const error = state[`${props.name}Error`]
+
+  const handleChange = (event: React.FocusEvent<HTMLInputElement>): void => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  const getStatus = (): string => error ? '🔴' : '🟢'
+
   return (
     <div className={S.inputWrapper}>
-      <input {...props}/>
+      <input {...props} onChange={handleChange}/>
     </div>
   )
 }
