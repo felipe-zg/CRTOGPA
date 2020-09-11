89cf52cbbd06706c05cf9729c561343de902a928
Felipe Zêba <51774420+felipe-zg@users.noreply.github.com>
2020-09-11 15:30:44 -0300

    test: ensure ValidationBuilder returns NumberValidation

--- src/validations/validators/builder/validation-builder.spec.ts
+++ src/validations/validators/builder/validation-builder.spec.ts
@@ -1,12 +1,22 @@
 import faker from 'faker'
 import { ValidationBuilder as sut } from '@/validations/validators/builder/validation-builder'
-import { RequiredFieldValidation, CRFormatValidation } from '@/validations/validators'
+import {
+  RequiredFieldValidation,
+  CRFormatValidation
+} from '@/validations/validators'
+import { NumberValidation } from '../number/number-validation'
 
 describe('ValidationBuilder', () => {
   it('should return RequiredFieldValidation', () => {
     const field = faker.database.column()
     const validators = sut.field(field).required().build()
     expect(validators).toEqual([new RequiredFieldValidation(field)])
+  })
+
+  it('should return NumberValidation', () => {
+    const field = faker.database.column()
+    const validators = sut.field(field).number().build()
+    expect(validators).toEqual([new NumberValidation(field)])
   })
 
   it('should return CRFormatValidation', () => {
