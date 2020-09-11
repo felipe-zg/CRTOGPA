ad1a8319f9767404c34871d6c078c06015f520c4
Felipe Zêba <51774420+felipe-zg@users.noreply.github.com>
2020-09-11 15:26:08 -0300

    test: ensure NumberValidation returns falsy if value is a valid number

--- src/validations/validators/number/number-validation.spec.ts
+++ src/validations/validators/number/number-validation.spec.ts
@@ -19,4 +19,16 @@ describe('CRFormatValidation', () => {
     const result = sut.validate('9..4')
     expect(result).toEqual(new InvalidCRFormatError())
   })
+
+  it('should return falsy if value is a valid number', () => {
+    const { sut } = makeSut()
+    const result1 = sut.validate(
+      String(faker.random.number({ min: 4.1, max: 4.9 }))
+    )
+    const result2 = sut.validate(
+      String(faker.random.number({ min: 1, max: 10 }))
+    )
+    expect(result1).toBeFalsy()
+    expect(result2).toBeFalsy()
+  })
 })
