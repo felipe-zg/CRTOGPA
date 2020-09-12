0502f2aa03ab68b1112d966435eb49df5dca740d
Felipe Zêba <51774420+felipe-zg@users.noreply.github.com>
2020-09-12 13:38:13 -0300

    feat: added a js colors file to use with styled-components

---
+++ src/presentation/styles/colors.js
@@ -0,0 +1,25 @@
+export const background = {
+  bgPrimary: '#fff',
+  bgSecondary: ' #ddd',
+  bgPrimaryDark: '#483D8B',
+  bgGreen: '#080',
+  bgBlue: '#00f',
+  bgRed: '#f00',
+  bgYellow: '#daa520'
+}
+
+export const text = {
+  white: '#fff',
+  black: '#000',
+  primary: '#483D8B',
+  primaryDark: '#560027',
+  primaryLight: '#bc477b',
+  red: '#f00'
+}
+
+export const button = {
+  backgroundEnabled: '#483D8B',
+  backgroundDisabled: ' #ddd',
+  colorEnabled: '#fff',
+  colorDisabled: '#666'
+}
