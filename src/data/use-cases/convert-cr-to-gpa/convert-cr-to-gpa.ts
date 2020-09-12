8f1e38ecced07d44b394842da6988318bd912147
Felipe Zêba <51774420+felipe-zg@users.noreply.github.com>
2020-09-12 14:50:27 -0300

    feat: added result style on converter result return

--- src/data/use-cases/convert-cr-to-gpa/convert-cr-to-gpa.ts
+++ src/data/use-cases/convert-cr-to-gpa/convert-cr-to-gpa.ts
@@ -1,15 +1,17 @@
 import { Converter } from '@/domain/use-cases'
+import { background } from '@/presentation/styles/colors'
 
 type Gpa = {
   scale: number
   grade: string
+  bg: string
 }
 
 export class ConvertCRToGPA implements Converter<number, Gpa> {
   gpa: Gpa
 
   constructor() {
-    this.gpa = { scale: 0, grade: '' }
+    this.gpa = { scale: 0, grade: '', bg: '' }
   }
 
   convert(cr: number): Gpa {
@@ -17,39 +19,51 @@ export class ConvertCRToGPA implements Converter<number, Gpa> {
     switch (true) {
       case this.gpa.scale >= 4.0:
         this.gpa.grade = 'A'
+        this.gpa.bg = background.bgGreen
         break
       case this.gpa.scale >= 3.7:
         this.gpa.grade = 'A-'
+        this.gpa.bg = background.bgGreen
         break
       case this.gpa.scale >= 3.3:
         this.gpa.grade = 'B+'
+        this.gpa.bg = background.bgBlue
         break
       case this.gpa.scale >= 3.0:
         this.gpa.grade = 'B'
+        this.gpa.bg = background.bgBlue
         break
       case this.gpa.scale >= 2.7:
         this.gpa.grade = 'B-'
+        this.gpa.bg = background.bgBlue
         break
       case this.gpa.scale >= 2.3:
         this.gpa.grade = 'C+'
+        this.gpa.bg = background.bgYellow
         break
       case this.gpa.scale >= 2.0:
         this.gpa.grade = 'C'
+        this.gpa.bg = background.bgYellow
         break
       case this.gpa.scale >= 1.7:
         this.gpa.grade = 'C-'
+        this.gpa.bg = background.bgYellow
         break
       case this.gpa.scale >= 1.3:
         this.gpa.grade = 'D+'
+        this.gpa.bg = background.bgRed
         break
       case this.gpa.scale >= 1.0:
         this.gpa.grade = 'D'
+        this.gpa.bg = background.bgRed
         break
       case this.gpa.scale >= 0.7:
         this.gpa.grade = 'D-'
+        this.gpa.bg = background.bgRed
         break
       default:
         this.gpa.grade = 'F'
+        this.gpa.bg = background.bgRed
     }
     return this.gpa
   }
