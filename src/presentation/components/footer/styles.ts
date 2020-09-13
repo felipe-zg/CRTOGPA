b6e51de4ba614b7ba2be29afb3543fcc5bb3fda4
felipe-zg <felipe_zeba@outlook.com>
2020-09-13 12:34:46 -0300

    refactor: added color to footer background

--- src/presentation/components/footer/styles.ts
+++ src/presentation/components/footer/styles.ts
@@ -1,13 +1,16 @@
 import styled from 'styled-components'
+import { background, text } from '@/presentation/styles/colors'
 
 export const Footer = styled.footer`
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 100cw;
+  width: 100vw;
   padding: 0.5rem 0 0.2rem 0;
+  background: ${background.bgPrimaryDark};
 
   p {
+    color: ${text.white};
     font-size: 0.8rem;
   }
 `
